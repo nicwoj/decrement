3763783388f8c77dfa82c3b933a6b671893ee01b
Nicole <nwoje13@gmail.com>
2020-06-02 22:47:22 -0400

    added decrement and increment buttons

---
+++ src/Decrement.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Decrement extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: props.start };
+  }
+
+  handleDecrement = (state) => {
+    if (state.number === 0) {
+      return alert("Cannot be less than zero!");
+    }
+    this.setState({ number: state.number - 1 });
+	};
+	
+	handleIncrement = (state) => {
+		this.setState({ number: state.number + 1});
+	}
+
+  render() {
+    return (
+      <div className="dec-inc">
+        {this.state.number}{" "}
+        <button onClick={() => this.handleDecrement(this.state)}>
+          decrement
+        </button>
+				<div class="divider1"/>
+				{" "}
+        <button onClick={() => this.handleIncrement(this.state)}>
+          increment
+        </button>
+      </div>
+    );
+  }
+}
+
+Decrement.propTypes = {
+  start: PropTypes.number.isRequired,
+};
+
+export default Decrement;
